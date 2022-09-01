f8de3387603a2edfcaefeb1aa114401c1ab19719
Gustavo Mathias <gmgustavom@gmail.com>
2022-09-01 16:38:14 -0300

    criando o header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,41 @@ import React, { Component } from 'react';
 import CharacterCard from '../components/CharacterCard';
 
 export default class Home extends Component {
+  // eslint-disable-next-line react/state-in-constructor
+  state = {
+    characterList: [],
+    isLoading: true,
+  };
+
+  componentDidMount() {
+    const fetchApi = async () => {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const data = await response.json();
+
+      this.setState({ isLoading: false, characterList: data.results });
+    };
+    fetchApi();
+  }
+
   render() {
+    const { characterList, isLoading } = this.state;
+
     return (
-      <div>
-        <CharacterCard
-          image="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          name="Rick Sanchez"
-          status="Alive"
-          gender="male"
-        />
-      </div>
+      !isLoading && (
+        <div className="card-area">
+          {
+            characterList.map((character) => (
+              <CharacterCard
+                key={character.id}
+                image={character.image}
+                name={character.name}
+                status={character.status}
+                gender={character.gender}
+              />
+            ))
+          }
+        </div>
+      )
     );
   }
 }
